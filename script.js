d16bd98051c854fa081b1ca27284883ed6c4e229
Gourab <gourabchatterjee623@gmail.com>
2022-08-14 21:03:13 +0530

    basic UI changes No JS change

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+// *** Select Elements
 const container = document.querySelector('.container');
 const button = document.querySelector('#button');
+//@ *** CSS Styling Parts
 document.body.style.display = container.style.display = button.style.display = 'flex';
 document.body.style.justifyContent = container.style.justifyContent = button.style.justifyContent = 'center';
 document.body.style.alignItems = container.style.alignItems = button.style.alignItems = 'center';
-
+// * Add Click Event to this button
 button.addEventListener('click', colorGenerator);
+window.addEventListener('keydown', (e) => {
+  if (e.key === ' ') {
+    colorGenerator();
+  }
+});
+//@ Call Function Globally for Generating random color on Page load
 colorGenerator();
-
+// *** Creating the Generating Colors Logic With Function
 function colorGenerator() {
   const p = button.querySelector('p');
   let colorArr = [
@@ -19,6 +27,7 @@ function colorGenerator() {
   container.style.backgroundColor = `rgb(${rgbValue})`;
   button.style.boxShadow = `0 0 10px 5px rgba(0, 0, 0, 0.2), inset 0 0 5px 7px rgba(${rgbValue},0.6)`;
   p.style.color = `rgb(${rgbValue})`;
+  container.previousElementSibling.children[0].children[0].style.color = `rgb(${rgbValue})`;
   function colorToHex(color) {
     let hexDecimal = color.toString(16);
     return hexDecimal.length == 1 ? '0' + hexDecimal : hexDecimal;
